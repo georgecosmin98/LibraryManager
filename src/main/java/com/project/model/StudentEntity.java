81669875d24f760ac886b1a9868c73d914d666e1
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-04-06 10:38:12 +0300

    Student entity commit

---
+++ src/main/java/com/project/model/StudentEntity.java
@@ -0,0 +1,46 @@
+package com.project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_student")
+public class StudentEntity {
+
+private String studentName;
+private String phoneNumber;
+private String address;
+private String emailAddress;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
