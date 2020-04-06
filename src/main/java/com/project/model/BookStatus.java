751f8e4395be28bedafe8738598c2f1066aa21c1
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-04-06 10:00:24 +0300

    First commit

---
+++ src/main/java/com/project/model/BookStatus.java
@@ -0,0 +1,12 @@
+package com.project.model;
+
+public enum  BookStatus {
+    AVAILABLE(0),
+    NOTAVAILABLE(1);
+
+    private final int bookStatusCode;
+
+    BookStatus(int bookStatusCode) {
+        this.bookStatusCode = bookStatusCode;
+    }
+}
