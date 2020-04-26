771d9972ad5b7df173fce0e9b29be5212dced468
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-04-26 13:02:06 +0300

    Link addBook with main stages

--- src/main/java/com/project/ui/main/mainController.java
+++ src/main/java/com/project/ui/main/mainController.java
@@ -1,7 +1,15 @@
 package com.project.ui.main;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,4 +20,15 @@ public class mainController implements Initializable {
     }
 
 
+    public void addBook(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/add_book.fxml"));
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("Add book menu");
+        stage.setScene(new Scene(parent));
+        stage.initModality(Modality.APPLICATION_MODAL); //Blocheaza parintele pana e inchisa scena copilului
+        stage.show();
+    }
+
+    public void listbook(ActionEvent actionEvent) {
+    }
 }
