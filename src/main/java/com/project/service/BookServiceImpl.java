8023479e2eb4e76790704ace74133baf73aa6a53
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-05-12 12:21:04 +0300

    Add logger into BookServiceImpl

--- src/main/java/com/project/service/BookServiceImpl.java
+++ src/main/java/com/project/service/BookServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.logging.Logger;
 
 @Service
 @Transactional
@@ -16,16 +17,28 @@ public class BookServiceImpl {
     @Resource
     private BookRepository bookRepository;
 
+    private static Logger logger;
+
+    static {
+        System.setProperty("java.util.logging.config.file",
+                "C:\\Users\\ylyho\\OneDrive\\Documente\\GitHub\\LibraryManager\\src\\main\\resources\\log.properties");
+        //must initialize loggers after setting above property
+        logger = Logger.getLogger(BookServiceImpl.class.getName());
+    }
+
     public BookEntity createBook(String isbn, String title, String bookAuthor, Date yearOfPublication, BookStatus status) {
         BookEntity newBook = new BookEntity(isbn, title, bookAuthor, yearOfPublication, status);
+        logger.info("Creating new book");
         return bookRepository.create(newBook);
     }
 
     public void deleteBook(String title) {
+        logger.info("Deleting book from database");
         bookRepository.deleteBook(title);
     }
 
     public void updateBookStatus(String isbn, BookStatus status) {
+        logger.info("Updating book status");
         bookRepository.updateBookStatus(isbn, status);
     }
 
