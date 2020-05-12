edfcfba76d17c1b9b5febb433af1c76879b6ce6a
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-05-12 13:42:44 +0300

    Add logger into AddStudentController

--- src/main/java/com/project/ui/addstudent/AddStudentController.java
+++ src/main/java/com/project/ui/addstudent/AddStudentController.java
@@ -2,6 +2,7 @@ package com.project.ui.addstudent;
 
 import com.project.alert.makeAlert;
 import com.project.service.StudentServiceImpl;
+import com.project.ui.addlibrarian.AddLibrarianController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,6 +16,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.net.URL;
 import java.text.ParseException;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 public class AddStudentController implements Initializable {
 
@@ -37,18 +39,30 @@ public class AddStudentController implements Initializable {
     @FXML
     private TextField email;
 
+    private static Logger logger;
+
+    static {
+        System.setProperty("java.util.logging.config.file",
+                "C:\\Users\\ylyho\\OneDrive\\Documente\\GitHub\\LibraryManager\\src\\main\\resources\\log.properties");
+        //must initialize loggers after setting above property
+        logger = Logger.getLogger(AddLibrarianController.class.getName());
+    }
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("library_application_context.xml");
+    StudentServiceImpl studentService = (StudentServiceImpl) context.getBean(StudentServiceImpl.class);
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
-    ApplicationContext context = new ClassPathXmlApplicationContext("library_application_context.xml");
-    StudentServiceImpl studentService = (StudentServiceImpl) context.getBean(StudentServiceImpl.class);
 
     public void addStudent(ActionEvent actionEvent) throws ParseException {
 
         if (sid.getText().isEmpty() || studentName.getText().isEmpty() || phoneNumber.getText().isEmpty()
                 || address.getText().isEmpty() || email.getText().isEmpty() ) {
             makeAlert.showMessageAlert("Please fill all fields!");
+            logger.warning("Not all fields were completed!");
             return;
         }
 
@@ -58,6 +72,7 @@ public class AddStudentController implements Initializable {
 
     public void close(ActionEvent actionEvent) {
         Stage stage = (Stage) mainController.getScene().getWindow();
+        logger.info("Closing stage");
         stage.close();
     }
 
