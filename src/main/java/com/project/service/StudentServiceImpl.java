018b1832539b5288d24a47802a4689d6c2c256ae
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-05-12 12:24:41 +0300

    Add logger into StudentServiceImpl

--- src/main/java/com/project/service/StudentServiceImpl.java
+++ src/main/java/com/project/service/StudentServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.logging.Logger;
 
 @Service
 @Transactional
@@ -14,14 +15,24 @@ public class StudentServiceImpl{
     @Resource
     private StudentRepository studentRepository;
 
+    private static Logger logger;
+
+    static {
+        System.setProperty("java.util.logging.config.file",
+                "C:\\Users\\ylyho\\OneDrive\\Documente\\GitHub\\LibraryManager\\src\\main\\resources\\log.properties");
+        //must initialize loggers after setting above property
+        logger = Logger.getLogger(StudentServiceImpl.class.getName());
+    }
 
     public StudentEntity createStudent(String sid, String studentName, String phoneNumber, String address, String emailAddress) {
         StudentEntity newStudent = new StudentEntity(sid, studentName, phoneNumber, address, emailAddress);
+        logger.info("Creating student");
         return studentRepository.create(newStudent);
     }
 
 
     public void deleteStudent(String sid) {
+        logger.info("Deleting student from database");
         studentRepository.deleteStudent(sid);
     }
 
