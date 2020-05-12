c64fd2dc2097f0165650f2571117324938952d8a
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-05-12 13:46:00 +0300

    Add logger into DeleteBookController

--- src/main/java/com/project/ui/deletebook/DeleteBookController.java
+++ src/main/java/com/project/ui/deletebook/DeleteBookController.java
@@ -18,6 +18,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import javax.persistence.NoResultException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 public class DeleteBookController implements Initializable {
 
@@ -30,33 +31,44 @@ public class DeleteBookController implements Initializable {
     @FXML
     private ListView<String> listView;
 
+    private static Logger logger;
+
+    static {
+        System.setProperty("java.util.logging.config.file",
+                "C:\\Users\\ylyho\\OneDrive\\Documente\\GitHub\\LibraryManager\\src\\main\\resources\\log.properties");
+        //must initialize loggers after setting above property
+        logger = Logger.getLogger(DeleteBookController.class.getName());
+    }
+
     ApplicationContext context = new ClassPathXmlApplicationContext("library_application_context.xml");
     BookServiceImpl bookService = (BookServiceImpl) context.getBean(BookServiceImpl.class);
 
     public void deleteBook(ActionEvent actionEvent) {
-        try{
-           if(bookService.getBookRepository().searchBookByISBN(ISBN.getText()).getStatus().equals(BookStatus.AVAILABLE))
-               bookService.getBookRepository().deleteBook(bookService.getBookRepository().searchBookByISBN(ISBN.getText()).getTitle());
-           else
-               makeAlert.showMessageAlert("This book is not available, please check if ISBN is correct or if this book has been returned!");
-        }catch (Exception ex){
-            System.out.println("This book ISBN do not exist into database!");
+        try {
+            if (bookService.getBookRepository().searchBookByISBN(ISBN.getText()).getStatus().equals(BookStatus.AVAILABLE))
+                bookService.getBookRepository().deleteBook(bookService.getBookRepository().searchBookByISBN(ISBN.getText()).getTitle());
+            else {
+                makeAlert.showMessageAlert("This book is not available, please check if ISBN is correct or if this book has been returned!");
+                logger.warning("This book is not available, please check if ISBN is correct or if this book has been returned!");
+            }
+        } catch (Exception ex) {
+            logger.severe("This book ISBN do not exist into database!");
             makeAlert.showMessageAlert("This book ISBN do not exist into database!");
         }
     }
 
     public void loadBookInfo(ActionEvent actionEvent) {
-        ObservableList<String>  bookData = FXCollections.observableArrayList();
-        try{
-            BookEntity bookEntity =bookService.getBookRepository().searchBookByISBN(ISBN.getText());
-            bookData.add ("Info about this book: \n");
-            bookData.add ("Book Author: " + bookEntity.getBookAuthor());
-            bookData.add ("Book Title: " + bookEntity.getTitle());
-            bookData.add ("Book year of publication: " + bookEntity.getYearOfPublication());
-            bookData.add ("Book status: " + bookEntity.getStatus());
-        }
-        catch(NoResultException ex){
+        ObservableList<String> bookData = FXCollections.observableArrayList();
+        try {
+            BookEntity bookEntity = bookService.getBookRepository().searchBookByISBN(ISBN.getText());
+            bookData.add("Info about this book: \n");
+            bookData.add("Book Author: " + bookEntity.getBookAuthor());
+            bookData.add("Book Title: " + bookEntity.getTitle());
+            bookData.add("Book year of publication: " + bookEntity.getYearOfPublication());
+            bookData.add("Book status: " + bookEntity.getStatus());
+        } catch (NoResultException ex) {
             makeAlert.showMessageAlert("This book do not exist into database!");
+            logger.severe("This book do not exist into database!");
         }
 
         listView.getItems().setAll(bookData);
