0c9a6a1082ffcdb55a327d9843a6fc2db4ebd6c3
georgecosmin98 <ilincageorgecomsin@yahoo.com>
2020-05-13 10:36:21 +0300

    Add unit test for BookBorrowServiceTest

--- src/test/java/com/project/service/BookBorrowServiceTest.java
+++ src/test/java/com/project/service/BookBorrowServiceTest.java
@@ -1,29 +1,46 @@
 package com.project.service;
 
+import com.project.model.BookBorrowStatus;
+import com.project.repository.api.BookBorrowRepository;
 import com.project.ui.settings.Settings;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.text.SimpleDateFormat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class BookBorrowServiceTest {
 
-    private BookBorrowServiceImpl bookBorrowService;
-    SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+    @Mock
+    BookBorrowRepository bookBorrowRepository;
+
+    @InjectMocks
+    BookBorrowServiceImpl bookBorrowService;
+
     @Before
-    public void setUp(){
-        this.bookBorrowService = new BookBorrowServiceImpl();
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void shouldReturnCorrectFine(){
+    public void shouldReturnCorrectFine() {
 
         Settings settings = Settings.getSettings();
         float numberOfDays = 2;
-        long expected = (long) (settings.getFinePerDay()*numberOfDays);
+        long expected = (long) (settings.getFinePerDay() * numberOfDays);
         long count = bookBorrowService.fineCalculator((long) numberOfDays);
-        Assert.assertEquals("Should return correct fine ammount: ",expected,count);
+        Assert.assertEquals("Should return correct fine ammount: ", expected, count);
     }
-    
+
+    @Test
+    public void shouldReturnCorrectUpdateStatus(){
+
+        bookBorrowService.updateBookBorrowStatus("1234", BookBorrowStatus.RETURNED);
+        verify(bookBorrowRepository,times(1)).updateBookBorrowStatus("1234", BookBorrowStatus.RETURNED);
+    }
+
 }
